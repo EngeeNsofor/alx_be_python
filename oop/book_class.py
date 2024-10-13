99ebb77a6ce7a12eda7e11aea33acf2a6676cb2c
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-10-13 18:19:27 +0100

    Created book_class.py and main.py files

---
+++ oop/book_class.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Constructor to initialize the Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor that is called when the object is about to be destroyed."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self) -> str:
+        """String representation of the Book instance."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self) -> str:
+        """Official representation of the Book instance for debugging."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
