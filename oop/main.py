030df22d3784d4cf0b4540d3bd0f931509473adb
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-10-13 18:32:43 +0100

    Added class_static_methods_demo.py file and updated main.py

--- oop/main.py
+++ oop/main.py
@@ -45,7 +45,6 @@ if __name__ == "__main__":
 
 
 #testing for polymorphism_demo.py
-
 from polymorphism_demo import Shape, Rectangle, Circle
 import math
 
@@ -57,6 +56,22 @@ def main():
 
     for shape in shapes:
         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+if __name__ == "__main__":
+    main()
+
+
+# testing for class_static_methods_demo.py
+from class_static_methods_demo import Calculator
+
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
